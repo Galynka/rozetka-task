ae9637f6a3caa8dd28d7c67cabea3e52a6e14f3a
galynashine <galyna.naumenko@gmail.com>
2017-06-26 22:27:48 +0200

    add entry point

---
+++ src/main/java/naumenko/RozetkaApp.java
@@ -0,0 +1,11 @@
+package naumenko;
+
+/**
+ * Created by galynashine on 6/26/17.
+ */
+public class RozetkaApp {
+
+    public static void main(String[] args) {
+        System.out.println("analysing rozetka.com ... uuu");
+    }
+}
