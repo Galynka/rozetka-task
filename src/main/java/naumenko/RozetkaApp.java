99f587771130767197a8f6b8032cbf3993d63eda
galynashine <galyna.naumenko@gmail.com>
2017-06-28 23:00:27 +0200

    added Selenium properties, added Selenium Tests

--- src/main/java/naumenko/RozetkaApp.java
+++ src/main/java/naumenko/RozetkaApp.java
@@ -1,11 +1,21 @@
 package naumenko;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by galynashine on 6/26/17.
  */
 public class RozetkaApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("analysing rozetka.com ... uuu");
+        BrowserDriver.loadPage("http://rozetka.com.ua/");
+        BrowserDriver.getCurrentDriver().findElement(By.xpath("//*[@id=\"3361\"]/a")).click();
+        BrowserDriver.getCurrentDriver().findElement(By.xpath("//*[@id=\"menu_categories_left\"]/li[1]/h4/a")).click();
+        BrowserDriver.getCurrentDriver().findElement(By.xpath("//*[@id=\"menu_categories_left\"]/li[1]/div/a[1]")).click();
+        BrowserDriver.getCurrentDriver().findElement(By.className("g-tag g-tag-icon-middle-popularity sprite")).click();
+
+        Thread.sleep(5000l);
     }
 }
